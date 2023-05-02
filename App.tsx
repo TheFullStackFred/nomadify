f8c50862a391b3149843d320588a22aee18495b9
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-05-02 22:04:35 +0200

    Fix splashscreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
 
-const App = () => {
+SplashScreen.preventAutoHideAsync()
+
+export const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+      } catch (error) {
+        console.warn(error)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Nomadify</Text>
     </View>
   )
 }
@@ -15,7 +43,6 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
