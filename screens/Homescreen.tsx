ee3f58172d77b3faef3a74a26e63687190ceea6f
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-05-03 04:39:28 +0200

    UI for register and login

---
+++ screens/Homescreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Homescreen = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  )
+}
+
+export default Homescreen
+
+const styles = StyleSheet.create({})
--- screens/Loginscreen.tsx
+++ screens/Loginscreen.tsx
@@ -1,5 +1,106 @@
+import { useState } from 'react'
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
 const Loginscreen = () => {
-  return <div>Loginscreen</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Email'
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        ></TextInput>
+        <TextInput
+          placeholder='Password'
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        ></TextInput>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <LinearGradient
+            colors={['#f4c4f3', '#fc67fa']}
+            style={styles.buttonGradient}
+            start={[0, 0]}
+            end={[1, 0]}
+          >
+            <Text style={styles.buttonText}>Login</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
 
 export default Loginscreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    width: '100%',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonGradient: {
+    width: '100%',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: '#fff',
+    marginTop: 5,
+    borderColor: '#fc67fa',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 16,
+    padding: 15,
+  },
+  buttonOutlineText: {
+    color: '#fc67fa',
+    fontWeight: '700',
+    fontSize: 16,
+    padding: 15,
+  },
+})
