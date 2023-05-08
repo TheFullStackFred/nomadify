e8158d9f958a7f8d74fa04a34e0a04bee22530c7
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-05-08 23:45:10 +0200

    Change to fade in

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -4,20 +4,20 @@ import { confettiStyles } from '../styles'
 import ConfettiCannon from 'react-native-confetti-cannon'
 
 const Confetti = () => {
-  const slideAnim = useRef(new Animated.Value(-100)).current
+  const fadeAnim = useRef(new Animated.Value(0)).current
 
   useEffect(() => {
-    Animated.timing(slideAnim, {
-      toValue: 0,
-      duration: 500,
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
       useNativeDriver: true,
     }).start()
-  }, [slideAnim])
+  }, [fadeAnim])
 
   return (
     <>
       <ConfettiCannon count={200} origin={{ x: -10, y: 0 }} fallSpeed={4000} />
-      <Animated.View style={{ transform: [{ translateX: slideAnim }] }}>
+      <Animated.View style={{ opacity: fadeAnim }}>
         <Text style={confettiStyles.text}>Travel Added</Text>
       </Animated.View>
     </>
